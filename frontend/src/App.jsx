b0ca5ffc0cc4522f901e82a1261a81391415f5f2
harshit960 <raj.harshit960@gmail.com>
2024-10-15 10:21:37 +0530

    Refactor package.json and package-lock.json to add react-router-dom v6.27.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import VideoTrans from "./pages/VideoTrans";
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <>
-    <VideoTrans />
+    
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<VideoTrans />} />
+          {/* <Route path="blogs" element={<Blogs />} /> */}
+          
+        
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
